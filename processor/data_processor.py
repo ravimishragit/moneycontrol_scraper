8f45b49e89131daa9bbf1ccd7300b03d5fe135a8
Ravi Kumar Mishra <ravimishrart2014@gmail.com>
2025-04-14 21:42:35 +0530

    fix for data processor

--- processor/data_processor.py
+++ processor/data_processor.py
@@ -8,19 +8,24 @@ class DataProcessor:
     """
 
     def __init__(self, data: pd.DataFrame):
-        self.data = data
+        self.data = data.copy()  # Avoid mutating original input
 
-    def filter_data(self):
-        """ Filters the data for the last 3 months and only 'Buy' and 'Sell' recommendations """
+    def filter_last_3_months(self) -> pd.DataFrame:
+        """Filter data from the last 3 months based on 'Reporting_Date'"""
         self.data['Reporting_Date'] = pd.to_datetime(self.data['Reporting_Date'])
-
-        # Filter last 3 months
         three_months_ago = datetime.today() - timedelta(days=90)
-        self.data = self.data[(self.data['Reporting_Date'] >= three_months_ago) &
-                              (self.data['Recommendation'].isin(['Buy', 'Sell']))]
+        filtered = self.data[self.data['Reporting_Date'] >= three_months_ago]
+        return filtered
 
-    def group_by_broker(self):
-        """ Group data by Broker and find the top recommendation based on Profit Potential """
+    def filter_by_recommendation(self, recommendations: list) -> pd.DataFrame:
+        """Filter data by given recommendation types (e.g., Buy, Sell)"""
+        filtered = self.data[self.data['Recommendation'].isin(recommendations)]
+        return filtered
+
+    def get_top_recommendations_by_broker(self) -> pd.DataFrame:
+        """
+        Group data by Broker and return the top recommendation based on Profit Potential.
+        """
         top_recommendations = (
             self.data.sort_values(['Broker', 'Profit_Potential'], ascending=[True, False])
             .groupby('Broker')
@@ -29,15 +34,20 @@ class DataProcessor:
         )
         return top_recommendations
 
-    def find_top_3(self, grouped_df: pd.DataFrame):
-        """ Finds the Top 3 Companies and Brokers based on Profit Potential """
+    def find_top_companies_and_brokers(self) -> tuple:
+        """
+        Finds the Top 3 Companies and Brokers based on Profit Potential.
+        Returns:
+            top_companies (DataFrame), top_brokers (DataFrame)
+        """
         top_companies = (
             self.data.sort_values('Profit_Potential', ascending=False)
             .head(3)[['Company', 'Profit_Potential']]
         )
 
+        broker_grouped = self.get_top_recommendations_by_broker()
         top_brokers = (
-            grouped_df.sort_values('Profit_Potential', ascending=False)
+            broker_grouped.sort_values('Profit_Potential', ascending=False)
             .head(3)[['Broker', 'Profit_Potential']]
         )
 
